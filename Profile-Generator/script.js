483cefe8cf57f04caecd2255ee3258fc07876a5c
andreii1907 <luchianandrei1024@yahoo.com>
2021-05-18 17:16:51 +0300

    Profile Generator

---
+++ Profile-Generator/script.js
@@ -0,0 +1,14 @@
+const profileImg = document.querySelectorAll('.profiles img');
+const profileName = document.querySelectorAll('.profiles p');
+const profileEmail = document.querySelectorAll('.profiles small')
+
+fetch('https://reqres.in/api/users')
+    .then(response => response.json())
+    .then(user => {
+        for(let i = 0; i < user.data.length; i++) {
+            profileImg.getAttribute = 'src';
+            profileImg[i].src = user.data[i].avatar;
+            profileName[i].textContent = `${user.data[i].first_name} ${user.data[i].last_name}`;
+            profileEmail[i].textContent = user.data[i].email;
+        }
+});
